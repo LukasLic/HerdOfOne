1d14a1753ed77e64e5d79a20192b0a6d61173927
xlicek01 <lukaslicek11@gmail.com>
2020-04-19 14:12:14 +0200

    Edited build settings, added graphics, fixed UI.

---
+++ Assets/Scripts/UiControls/ResolutionChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionChange : MonoBehaviour
+{
+    private bool usingSmallRes = true;
+
+    public Vector2 smallResolution;
+    public Vector2 bigResolution;
+
+
+    public void SwitchResolution()
+    {
+        if(usingSmallRes)
+        {
+            SetRes(bigResolution);
+            usingSmallRes = false;
+        }
+        else
+        {
+            SetRes(smallResolution);
+            usingSmallRes = true;
+        }
+    }
+
+    private void SetRes(Vector2 newResolution)
+    {
+        Debug.Log($"Changing resolution to: {(int)newResolution.x}x{(int)newResolution.y}.");
+        Screen.SetResolution((int)newResolution.x, (int)newResolution.y, Screen.fullScreen);
+    }
+}
